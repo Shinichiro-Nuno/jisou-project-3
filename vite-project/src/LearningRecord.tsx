b31963ed9a00c8efe611ffc98019b02efb003d38
Shinichiro-Nuno <nuno1216shin@gmail.com>
2024-12-07 15:10:12 +0900

    削除ボタン作成

--- vite-project/src/LearningRecord.tsx
+++ vite-project/src/LearningRecord.tsx
@@ -3,6 +3,7 @@ import { supabase } from "../lib/supabase-client";
 
 function LearningRecord() {
   type Record = {
+    id: number;
     title: string;
     time: number;
   };
@@ -14,15 +15,51 @@ function LearningRecord() {
   const [isLoading, setIsLoading] = useState(true);
 
   const onClickSave = useCallback(
-    (title: string, time: number) => {
+    async (title: string, time: number) => {
       if (title && time) {
-        const newRecords = [...records, { title, time }];
-        setRecords(newRecords);
-        setTitle("");
-        setTime(0);
-        setIsInput(true);
+        const { data, error } = await supabase
+          .from("study-record")
+          .insert({ title, time })
+          .select();
+
+        if (error) {
+          console.error("データ登録エラー:", error.message);
+          return;
+        }
+
+        if (data && data.length > 0) {
+          console.log(data);
+          const insertedRecord = data[0];
+
+          const newRecords = [...records, insertedRecord];
+          setRecords(newRecords);
+          setTitle("");
+          setTime(0);
+          setIsInput(true);
+        }
       } else {
         setIsInput(false);
+      }
+    },
+    [records]
+  );
+
+  const onClickDelete = useCallback(
+    async (recordId: number, index: number) => {
+      if (recordId) {
+        const { error } = await supabase
+          .from("study-record")
+          .delete()
+          .eq("id", recordId);
+
+        if (error) {
+          console.error("データ削除エラー:", error.message);
+          return;
+        }
+
+        const newRecords = [...records];
+        newRecords.splice(index, 1);
+        setRecords(newRecords);
       }
     },
     [records]
@@ -76,7 +113,15 @@ function LearningRecord() {
           <p>Loading...</p>
         ) : (
           records.map((record, index) => (
-            <li key={index}>{`${record.title} ${record.time}時間`}</li>
+            <li key={index}>
+              {`${record.title} ${record.time}時間`}
+              <button
+                onClick={() => onClickDelete(record.id, index)}
+                style={{ marginLeft: "8px", marginBottom: "8px" }}
+              >
+                削除
+              </button>
+            </li>
           ))
         )}
       </ul>
