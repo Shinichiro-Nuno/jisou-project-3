be4d92fec8f0c49d566314306887f353bff48583
Shinichiro-Nuno <99865549+Shinichiro-Nuno@users.noreply.github.com>
2024-12-08 10:28:49 +0900

    Revert "Project 2 state"

--- vite-project/jest.setup.ts
+++
@@ -1,7 +0,0 @@
-import "@testing-library/jest-dom";
-
-require("dotenv").config();
-
-global.structuredClone = (val) => {
-  return JSON.parse(JSON.stringify(val));
-};
--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import env from "vite-plugin-env-compatible";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), env({ prefix: "VITE", mountedPath: "process.env" })],
-});
+  plugins: [react()],
+})
