656c389b5d6913e882b5755e507e6d5930b86c75
Shinichiro-Nuno <nuno1216shin@gmail.com>
2024-12-10 21:55:53 +0900

    chakra ui スニペット追加

--- src/__tests__/AppComponent.spec.tsx
+++ src/__tests__/AppComponent.spec.tsx
@@ -1,10 +1,15 @@
 import { render, screen } from "@testing-library/react";
 
 import LearningRecord from "../LearningRecord";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 
 describe("LearningRecord", () => {
   test("タイトルの確認", () => {
-    render(<LearningRecord />);
+    render(
+      <ChakraProvider value={defaultSystem}>
+        <LearningRecord />
+      </ChakraProvider>
+    );
     expect(screen.getByText("学習記録一覧")).toBeInTheDocument();
   });
 });
