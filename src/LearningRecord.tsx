fb60b137b9e47d3b3d70dc5ba0c561487f1bacba
Shinichiro-Nuno <nuno1216shin@gmail.com>
2024-12-10 21:19:40 +0900

    chakra ui インストール

--- src/LearningRecord.tsx
+++ src/LearningRecord.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useState } from "react";
-import { supabase } from "../lib/supabase-client";
+import { supabase } from "./lib/supabase-client";
 
 function LearningRecord() {
   type Record = {
